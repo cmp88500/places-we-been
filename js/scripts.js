a08d15bb4df0dcc71c455eb623c4b76b789294e3
Cam Peters and Joe Friesen <joe@>
2018-08-20 10:35:16 -0700

    Add function to store values entered by user on form to new object newLocation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
-function Location(city, state, country) {
+// Business logic
+
+function Location(city, state, country, date, landmark, notes) {
   this.city = city;
   this.state = state;
   this.country = country;
+  this.dateVisited = date;
+  this.landmark = landmark;
+  this.notes = notes;
 }
 
 Location.prototype.title = function() {
   return this.city + " " + this.state + " " + this.country;
 }
+
+
+
+// User interface logic
+
+$(document).ready(function() {
+  $("form#places").submit(function(event) {
+    event.preventDefault();
+
+    var newCity = $("#city").val();
+    var newState = $("#state").val();
+    var newCountry = $("#country").val();
+
+    var newLocation = Location(newCity, newState, newCountry, "", "", "");
+  })
+});
